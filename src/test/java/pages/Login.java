9455e23b54991c376fa408d79ac652f4772f06da
Mihir992 <119054373+Mihir992@users.noreply.github.com>
2023-08-26 11:08:11 +0530

    Update Login.java

--- src/test/java/pages/Login.java
+++ src/test/java/pages/Login.java
@@ -3,9 +3,10 @@
  */
 package pages;
 
-import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
 
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
@@ -16,40 +17,45 @@ import stepDefinitions.LoginStepDefs;
 /**
  * 
  */
-public class Login{
+public class Login extends BasePage{
 
-	WebDriver driver = new ChromeDriver();
-	LoginStepDefs login = new LoginStepDefs(driver);
-	
-	
-	@Given("user on the Login Page")
-	public void user_on_the_Login_Page() {
-		driver.manage().window().maximize();
-		login.openLoginPage("https://automationteststore.com/");
+	public Login(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
 	}
 
-	@Then("Verify page title {string}")
-	public void Verify_page_title(String expAccountLoginTitle) {
-		String actAccountLoginPageTitle = login.getPageTitle();
-		Assert.assertEquals(actAccountLoginPageTitle,expAccountLoginTitle,"Page Title is not getting matched");
+	
+	//private LoginStepDefs login = new LoginStepDefs(driver);
+	
+	@FindBy(xpath="//input[@id='loginFrm_loginname']")
+	private WebElement username;
+	
+
+	@FindBy(xpath="//div[@class='form-group']//input[@id='loginFrm_password']")
+	private WebElement password;
+	
+
+	@FindBy(xpath="//form[@id='loginFrm']/fieldset/button")
+	private WebElement loginButton;
+	
+	public void openLoginPage(String expURL) {
+		openURL(expURL);
 	}
 
-	@When("user enters username {string} and password {string}")
-	public void  user_enters_username_and_password(String uid,String pwd){
-		login.enterUsername(uid);
-		login.enterPassword(pwd);
-		
+	public void enterUsername(String uname) {
+		enterText(username,uname);
 	}
 	
-	@And("user clicks on Login button")
-	public void user_clicks_on_Login_button() {
-		login.user_click_Login_button();
+	
+	public void enterPassword(String passwd) {
+		enterText(password,passwd);
 	}
 	
-	@Then("verify page title {string}")
-	public void verify_page_title(String expMyAccountTitle) {
-		String actMyAccountPageTitle = login.getPageTitle();
-		Assert.assertEquals(actMyAccountPageTitle,expMyAccountTitle,"Page Title is not getting matched");
+	public void user_click_Login_button() {
+		click(loginButton);
 	}
 	
+	public void getLoginPageTitle() {
+		getTitle(driver);
+	}	
 }
