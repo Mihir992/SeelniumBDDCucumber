1d2cc46cd255d60299c85e10746f423c0402a1e9
Mihir992 <119054373+Mihir992@users.noreply.github.com>
2023-10-01 18:28:16 +0530

    Update TestNGRunnerTest.java

--- src/test/java/runners/TestNGRunnerTest.java
+++ src/test/java/runners/TestNGRunnerTest.java
@@ -1,15 +1,44 @@
-package runners; 
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
+package com.cucumber.runners;
 
-import static io.cucumber.testng.CucumberOptions.SnippetType.CAMELCASE;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.CucumberFeatureWrapper;
+import cucumber.api.testng.PickleEventWrapper;
+import cucumber.api.testng.TestNGCucumberRunner;
 
  @CucumberOptions(
-		 plugin = {"pretty","html:target/cucumber.html","summary"},
-		 snippets = CAMELCASE,
-		 dryRun = true,
-		 glue = "stepDefinitions",
-		 features = "src/test/resources/feature"
-		 )
- public class TestNGRunnerTest extends AbstractTestNGCucumberTests{  
+		 //plugin = {"pretty","html:target/site/cucumber-pretty","json:target/cucumber.json"},
+		 //plugin= {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+		 //snippets = CAMELCASE,
+		 //dryRun = true,
+		 features = "src/test/resources/feature",
+		 glue = {"stepDefinitions"},
+		 tags = {"@TestngTest"})
+ public class TestNGRunnerTest{
+	 private TestNGCucumberRunner testNGCucumberRunner;
+	 
+	 @BeforeClass(alwaysRun = true)
+	 public void setUpClass() {
+		 testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+	 }
+	 
+	 @Test(dataProvider = "features")
+	 public void feature(PickleEventWrapper eventwrapper,CucumberFeatureWrapper cumcumberFeature) throws Throwable {
+		 //testNGCucumberRunner.runCucumber(cumcumberFeature.getCucumberFeature());
+		 testNGCucumberRunner.runScenario(eventwrapper.getPickleEvent());
+	 }
+	 
+	 @DataProvider
+	 public Object[][] features(){
+		 return testNGCucumberRunner.provideScenarios();
+	 }
+	 
+	 @AfterClass(alwaysRun = true)
+	 public void tearDownClass() {
+		 testNGCucumberRunner.finish();
+	 }
  }
