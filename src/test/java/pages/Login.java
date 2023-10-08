6b2f3e671fbd06f2f53cbe625a87be203c44812a
Mihir992 <119054373+Mihir992@users.noreply.github.com>
2023-10-08 11:51:36 +0530

    UpdateLogin_8thOctober2023.java

--- src/test/java/pages/Login.java
+++ src/test/java/pages/Login.java
@@ -1,32 +1,25 @@
 /**
  * 
  */
-package pages;
+package com.cucumber.pageObject;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
-import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
-import stepDefinitions.LoginStepDefs;
 
-/**
- * 
- */
 public class Login extends BasePage{
-
+	WebDriver driver;
+	
 	public Login(WebDriver driver) {
 		super(driver);
+		this.driver = driver;
+		driver.get("https://automationteststore.com/index.php?rt=account/login");
+		PageFactory.initElements(driver,this);
 		// TODO Auto-generated constructor stub
 	}
 
-	
-	//private LoginStepDefs login = new LoginStepDefs(driver);
-	
 	@FindBy(xpath="//input[@id='loginFrm_loginname']")
 	private WebElement username;
 	
@@ -38,14 +31,31 @@ public class Login extends BasePage{
 	@FindBy(xpath="//form[@id='loginFrm']/fieldset/button")
 	private WebElement loginButton;
 	
+	@FindBy(xpath="//a[@title='Continue']")
+	private WebElement ContinueButton;
+	
+	@FindBy(xpath="//i[@class='fa fa-lock fa-fw']")
+	private WebElement logoffButton;
+	
+	//@FindBy(xpath="//div[@class='alert alert-error alert-danger']")
+	//private WebElement validationMessage;
+	
+	
 	public void openLoginPage(String expURL) {
 		openURL(expURL);
 	}
 
+	public void clickOnUsernameTextField() {
+		txtElementClick(username);
+	}
+	
 	public void enterUsername(String uname) {
 		enterText(username,uname);
 	}
 	
+	public void clickOnPasswordTextField() {
+		txtElementClick(password);
+	}
 	
 	public void enterPassword(String passwd) {
 		enterText(password,passwd);
@@ -55,7 +65,24 @@ public class Login extends BasePage{
 		click(loginButton);
 	}
 	
+	public void user_click_Logoff_button() {
+		click(logoffButton);
+	}
+	
+	public void user_click_Continue_button() {
+		click(ContinueButton);
+	}
+	
+	/*public void getvalidationMessageText()
+	{
+		validationMessage.getText();
+	}*/
+	
 	public String getLoginPageTitle() {
 		return driver.getTitle();
-	}	
+	}
+	
+	public String getLogoutPageTitle() {
+		return driver.getTitle();
+	}
 }
