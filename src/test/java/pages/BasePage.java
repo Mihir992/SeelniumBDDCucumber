a51a1868c3b1f0a717f71d43646d5517f27ea560
Mihir992 <119054373+Mihir992@users.noreply.github.com>
2023-10-08 11:52:10 +0530

    UpdateBasePage_8thOctober2023.java

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,30 +1,36 @@
-package pages;
+package com.cucumber.pageObject;
 
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 
 public class BasePage {
 	
 	WebDriver driver;
-
-
+	
 	public BasePage(WebDriver driver) {
 		this.driver = driver;
+		// TODO Auto-generated constructor stub
 	}
-	
+
 	//common methods
 	public void openURL(String url) {
-		driver.manage().window().maximize();
-		driver.get(url);
-		
+		driver.get(url);	
 	}
 	
 	public static void enterText(WebElement elm,String text) {
+		//elm.clear(text);
+		elm.clear();
 		elm.sendKeys(text);
 	}
+	
+	public void txtElementClick(WebElement txtelement) {
+		txtelement.click();
+	}
+	
 	
 	public static void click(WebElement elm) {
 		elm.click();
@@ -33,10 +39,7 @@ public class BasePage {
 	public static void clickOnLink(WebElement linkelm) {
 		linkelm.click();
 	}	
-	public static void enterValue(WebElement elmValue,String qtyValue) {
-		int qty= Integer.parseInt(qtyValue);
-		elmValue.sendKeys(String.valueOf(qty));
-	}
+	
 	public String getTitle(WebDriver driver) {
 		boolean flag = false;
 
@@ -55,6 +58,11 @@ public class BasePage {
 		actions.moveToElement(elm1).click(elm2).build().perform();
 		
 	}
+	
+	/*public static void enterValue(WebElement elmValue,String qtyValue) {
+		int qty= Integer.parseInt(qtyValue);
+		elmValue.sendKeys(String.valueOf(qty));
+	}*/
 	
 	public void validateText(WebElement elmText,String expectedText) {
 		String actualText = elmText.getText();
